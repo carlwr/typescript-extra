f1ba424d59f2f656eea5eb719d8881465d4a7b77
Carl <carl@wernhoff.com>
2026-04-02 21:34:10 +0200

    build: add CJS export (is now ESM+CJS)

--- build.ts
+++ build.ts
@@ -19,7 +19,7 @@ async function buildProject() {
     entry: {
       'index': 'src/index.ts'
     },
-    format: ['esm'],
+    format: ['esm', 'cjs'],
     dts: true,
     sourcemap: true,
     clean: true,
